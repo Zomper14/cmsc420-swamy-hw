332c922742edbc07064b94c0495e137184daf0b0
Zomper14 <123605123+Zomper14@users.noreply.github.com>
2024-09-22 14:00:07 -0400

    Update ValleyTraveler.java

--- hw1/ValleyTraveler.java
+++ hw1/ValleyTraveler.java
@@ -8,6 +8,9 @@ public class ValleyTraveler {
 
     // Create instance variables here.
 
+    private int[] landscape;
+    private int size; 
+
     /**
      * Constructor to initialize the magical map with the given landscape of
      * Numerica.
@@ -15,7 +18,11 @@ public class ValleyTraveler {
      * @param landscape An array of distinct integers representing the landscape.
      */
     public ValleyTraveler(int[] landscape) {
-        // TODO: Implement the constructor.
+        this.landscape = new int[landscape.length];
+        for (int i = 0; i < landscape.length; i++) {
+            this.landscape[i] = landscape[i];
+        }
+        this.size = landscape.length;
     }
 
     /**
@@ -25,8 +32,8 @@ public class ValleyTraveler {
      * @return true if the landscape is empty, false otherwise.
      */
     public boolean isEmpty() {
-        // TODO: Implement the isEmpty method.
-        return false;
+        
+        return size == 0;
     }
 
     /**
@@ -35,7 +42,11 @@ public class ValleyTraveler {
      * @return The first valley point in the landscape.
      */
     public int getFirst() {
-        // TODO: Implement the getFirst method.
+        for (int i = 0; i < size; i++) {
+            if (isValley(i)) {
+                return landscape[i];
+            }
+        }
         return -1;
     }
 
@@ -45,8 +56,17 @@ public class ValleyTraveler {
      * @return The excavated valley point.
      */
     public int remove() {
-        // TODO: Implement the remove method.
-        return -1;
+        int valley = getFirst();
+        int[] newLandscape = new int[size - 1];
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            if (landscape[i] != valley) {
+                newLandscape[index++] = landscape[i];
+            }
+        }
+        landscape = newLandscape;
+        size--;
+        return valley;
     }
 
     /**
@@ -55,7 +75,42 @@ public class ValleyTraveler {
      * @param num The height of the new landform.
      */
     public void insert(int height) {
-        // TODO: Implement the insert method.
+        int valleyIndex = findFirstValleyIndex();
+        int[] newLandscape = new int[size + 1];
+        for (int i = 0; i < valleyIndex; i++) {
+            newLandscape[i] = landscape[i];
+        }
+        newLandscape[valleyIndex] = height;
+        for (int i = valleyIndex; i < size; i++) {
+            newLandscape[i + 1] = landscape[i];
+        }
+        landscape = newLandscape;
+        size++;
+    }
+
+
+
+
+    //helper methods
+    private boolean isValley(int i) {
+        if (i == 0 && size == 1) {
+            return true; // Only one element
+        } else if (i == 0) {
+            return landscape[i] < landscape[i + 1];
+        } else if (i == size - 1) {
+            return landscape[i] < landscape[i - 1];
+        } else {
+            return landscape[i] < landscape[i - 1] && landscape[i] < landscape[i + 1];
+        }
+    }
+
+    private int findFirstValleyIndex() {
+        for (int i = 0; i < size; i++) {
+            if (isValley(i)) {
+                return i;
+            }
+        }
+        return -1; // This should never happen
     }
 
 }
